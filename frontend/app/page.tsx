876a23cbf1715990d5530716803bae3a7ff785a8
Nyambuu <tnyambuu@gmail.com>
2025-12-23 18:25:31 +0800

    update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 type Person = {
   id: number;
   name: string;
+  type: "boy" | "girl";
 };
 
 type Pair = {
@@ -14,25 +15,128 @@ type Pair = {
 };
 
 export default function Home() {
-  const [teamA, setTeamA] = useState<Person[]>([]);
-  const [teamB, setTeamB] = useState<Person[]>([]);
-  const [pairs, setPairs] = useState<Pair[]>([]);
+  const [teamA, setTeamA] = useState<Person[]>([
+    {
+      id: 1,
+      name: "Boy 1",
+      type: "boy",
+    },
+    {
+      id: 2,
+      name: "Boy 2",
+      type: "boy",
+    },
+    {
+      id: 3,
+      name: "Boy 3",
+      type: "boy",
+    },
+    {
+      id: 4,
+      name: "Boy 4",
+      type: "boy",
+    },
+    {
+      id: 5,
+      name: "Boy 5",
+      type: "boy",
+    },
+    {
+      id: 6,
+      name: "Boy 6",
+      type: "boy",
+    },
+    {
+      id: 7,
+      name: "Boy 7",
+      type: "boy",
+    },
+    {
+      id: 8,
+      name: "Boy 8",
+      type: "boy",
+    },
+    {
+      id: 9,
+      name: "Boy 9",
+      type: "boy",
+    },
+    {
+      id: 10,
+      name: "Boy 10",
+      type: "boy",
+    },
+  ]);
+  const [teamB, setTeamB] = useState<Person[]>([
+    {
+      id: 1,
+      name: "Girl 1",
+      type: "girl",
+    },
+    {
+      id: 2,
+      name: "Girl 2",
+      type: "girl",
+    },
+    {
+      id: 3,
+      name: "Girl 3",
+      type: "girl",
+    },
+    {
+      id: 4,
+      name: "Girl 4",
+      type: "girl",
+    },
+    {
+      id: 5,
+      name: "Girl 5",
+      type: "girl",
+    },
+    {
+      id: 6,
+      name: "Girl 6",
+      type: "girl",
+    },
+    {
+      id: 7,
+      name: "Girl 7",
+      type: "girl",
+    },
+    {
+      id: 8,
+      name: "Girl 8",
+      type: "girl",
+    },
+    {
+      id: 9,
+      name: "Girl 9",
+      type: "girl",
+    },
+    {
+      id: 10,
+      name: "Girl 10",
+      type: "girl",
+    },
+  ]);
   const [rounds, setRounds] = useState<Pair[][]>([]);
   const [nextId, setNextId] = useState(1);
   const [inputA, setInputA] = useState("");
   const [inputB, setInputB] = useState("");
+  const [opponentBoys, setOpponentBoys] = useState<any>({});
+  const [opponentGirls, setOpponentGirls] = useState<any>({});
 
   const addToTeamA = () => {
     const trimmed = inputA.trim();
     if (!trimmed) return;
-    setTeamA((prev) => [...prev, { name: trimmed } as Person]);
+    setTeamA((prev) => [...prev, { id: prev.length + 1, name: trimmed, type: "boy" } as Person]);
     setInputA("");
   };
 
   const addToTeamB = () => {
     const trimmed = inputB.trim();
     if (!trimmed) return;
-    setTeamB((prev) => [...prev, { name: trimmed } as Person]);
+    setTeamB((prev) => [...prev, { id: prev.length + 1, name: trimmed, type: "girl" } as Person]);
     setInputB("");
   };
 
@@ -76,13 +180,13 @@ export default function Home() {
     if (teamA.length > teamB.length) {
       const diff = teamA.length - teamB.length;
       for (let i = 0; i < diff; i++) {
-        setTeamB((prev) => [...prev, { name: `Person ${nextId} (N/A)` } as Person]);
+        setTeamB((prev) => [...prev, { id: prev.length + 1, name: `Girl ${nextId} (N/A)` } as Person]);
         setNextId((prev) => prev + 1);
       }
     } else if (teamA.length < teamB.length) {
       const diff = teamB.length - teamA.length;
       for (let i = 0; i < diff; i++) {
-        setTeamA((prev) => [...prev, { name: `Person ${nextId} (N/A)` } as Person]);
+        setTeamA((prev) => [...prev, { id: prev.length + 1, name: `Boy ${nextId} (N/A)` } as Person]);
         setNextId((prev) => prev + 1);
       }
     }
@@ -103,20 +207,49 @@ export default function Home() {
         });
       }
       roundPairs.push(pair);
-      b.unshift(a.pop() as Person);
+      b.unshift(b.pop() as Person);
     }
 
     setRounds(roundPairs);
 
+    let opponentBoysTemp: any = {};
+    let opponentGirlsTemp: any = {};
+
+    for (let i = 0; i < teamA.length; i++) {
+      opponentBoys[teamA[i].id] = {
+        info: teamA[i],
+        opponents: [],
+      };
+    }
+
+    for (let i = 0; i < teamB.length; i++) {
+      opponentGirls[teamB[i].id] = {
+        info: teamB[i],
+        opponents: [],
+      };
+    }
+
+    for (let i = 0; i < roundPairs.length; i++) {
+      for (let j = 0; j < roundPairs[i].length; j++) {
+        opponentBoys[roundPairs[i][j].boy.id].opponents.push(roundPairs[i][j].girl);
+        opponentGirls[roundPairs[i][j].girl.id].opponents.push(roundPairs[i][j].boy);
+      }
+    }
+
+    setOpponentBoys(opponentBoysTemp);
+    setOpponentGirls(opponentGirlsTemp);
+
+    console.log(opponentBoys);
+    console.log(opponentGirls);
+
     console.log(roundPairs);
-    
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-zinc-50 dark:bg-black px-4">
       <main className="w-full max-w-4xl rounded-2xl bg-white dark:bg-zinc-900 shadow-lg p-6 sm:p-8">
         <h1 className="text-2xl sm:text-3xl font-semibold mb-2 text-zinc-900 dark:text-zinc-50 text-center">
-          Two-Team
+          Speed Date Generator
         </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 border-t border-zinc-200 dark:border-zinc-800 pt-6 mt-2">
@@ -292,7 +425,6 @@ export default function Home() {
                     <tbody>
                       {
                         round.map((pair: any, index) => {
-                          console.log(pair)
                           return (
                             <tr
                               key={index}
